bcfdc7af5ff43694325e948f7ca36ba2a4f8fbc3
kpetrunin <kostya.petrunin@gmail.com>
2023-09-28 15:51:21 +0200

    edited SQL requests

--- BD.sql
+++ BD.sql
@@ -12,7 +12,6 @@ SELECT track,
         WHEN finished = true THEN '2'
         WHEN cancelled = true THEN '-1'
         WHEN "inDelivery" = true THEN '1'
-        WHEN cancelled = true THEN '-1'
         ELSE '0'
     END
 FROM "Orders";
